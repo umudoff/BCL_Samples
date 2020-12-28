9a9a50b929fe0b647365d5c358c375a3235bcebd
umudoff <u.amrah@gmail.com>
2020-12-28 23:02:20 +0400

    Add project files.

---
+++ BCL_Samples/FileUtilities.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BCL_Samples
+{
+    class FileUtilities
+    {
+
+        public static void ProcessFile( string source)
+        {
+            
+            var fInfo = new FileInfo(source);
+            Console.WriteLine(string.Format(BCL_Samples.Resources.LogMessages.FileInfoMessage, fInfo.Name, fInfo.CreationTime)
+               );
+            
+            var folder= Path.Combine(fInfo.Directory.ToString(), "default");
+            var destination= Path.Combine(folder, fInfo.Name);
+            MoveFile(fInfo.FullName.ToString(), destination);
+
+
+        }
+        public static void MoveFile(string source, string destination)
+        {
+             
+            try
+            {
+                File.Move(source, destination);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (File.Exists(destination))
+            {
+                Console.WriteLine(string.Format(BCL_Samples.Resources.LogMessages.FileMovedSuccessMessage, source, destination));
+            }
+            else
+            {
+                Console.WriteLine(string.Format(BCL_Samples.Resources.LogMessages.FileMovedFailureMessage, source, destination));
+            }
+
+        }
+
+        
+    }
+}
---
+++ BCL_Samples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace BCL_Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var culture = new CultureInfo("ru-RU");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+           
+            var rm = new ResourceManager("BCL_Samples.Resources.LogMessages",typeof(Program).Assembly);
+            var ca = DateTime.Now;
+            
+            var mes = BCL_Samples.Resources.LogMessages.StopExecution;
+            Console.WriteLine(rm.GetString("StopExecution"));
+
+            var watcher = new FileSystemWatcher();
+            watcher.Path= @"C:\test_202012";
+            watcher.NotifyFilter = NotifyFilters.FileName;
+            watcher.Created+= (object sender, FileSystemEventArgs e)=> {
+                  FileUtilities.ProcessFile(e.FullPath);
+             };
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.ReadLine();
+
+        }
+
+        
+
+    }
+}
